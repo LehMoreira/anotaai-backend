fe25e7a0357c5b5de5b3749bb9bad2b1c3ea4ab4
Leticya <moreiraleticya23@gmail.com>
2026-06-11 20:49:10 -0300

    Update Usuario.java

--- src/main/java/br/com/anotaai/entities/Usuario.java
+++ src/main/java/br/com/anotaai/entities/Usuario.java
@@ -1,33 +1,60 @@
-package br.com.anotaai.entities;
+package br.com.anotaai.models;
 
 import br.com.anotaai.enums.Roles;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "users")
+@Table(name = "usuarios")
 @Getter
 @Setter
-@EqualsAndHashCode(of = "id")
-@NoArgsConstructor
-
 public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String nome;
-
+    private String login;
     private String email;
-
-    private String senha;
-
-
-    @Enumerated(EnumType.STRING)
+    private String password;
     private Roles role;
-
-
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Roles getRole() {
+		return role;
+	}
+	public void setRole(Roles role) {
+		this.role = role;
+	}
+    
+    
+	
+    
+    
 }
